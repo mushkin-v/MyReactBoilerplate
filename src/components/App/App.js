82847f43a4651375aa4886cf7c6c8afde0392220
Vitaliy <vitaliy.mushkin@mev.com>
2018-06-17 16:06:09 +0300

    add bootstrap

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import * as routes from '../../constants/routes';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import Landing from '../Landing';
 import Home from '../Home';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="app">
+        <div className="App container">
+          <div className="row">
+            <div className="col-12">
+              <ul>
+                <li><Link to={routes.LANDING}>Landing</Link></li>
+                <li><Link to={routes.HOME}>Home</Link></li>
+              </ul>
+            </div>
+          </div>
 
-          <ul>
-            <li><Link to={routes.LANDING}>Landing</Link></li>
-            <li><Link to={routes.HOME}>Home</Link></li>
-          </ul>
-
-          <hr/>
-
-          <Route exact path={routes.LANDING} component={() => <Landing />} />
-          <Route exact path={routes.HOME} component={() => <Home />} />
-
-          <hr/>
-
+          <div className="row">
+            <div className="col-12">
+              <Route exact path={routes.LANDING} component={() => <Landing />} />
+              <Route exact path={routes.HOME} component={() => <Home />} />
+            </div>
+          </div>
         </div>
-      </Router>
     );
   }
 }
